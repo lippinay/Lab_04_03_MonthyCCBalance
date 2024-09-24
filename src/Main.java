d314512053d6ecc66d86ac768bc644205ca138d3
Alyk Lippincott <lippinay@mail.uc.edu>
2024-09-24 17:20:28 -0400

    Task 3 completed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double balance = 5000;
+        double interestRate = 0.17;
+        double firstMonthInterest = balance * interestRate;
+        double secondMonthInterest = (balance + firstMonthInterest) * interestRate;
+        System.out.println("Interest after one month: $" + firstMonthInterest);
+        System.out.println("Interest after two months: $" + secondMonthInterest);
+
     }
 }
